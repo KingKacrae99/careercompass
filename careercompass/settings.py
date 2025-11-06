8e45e9ea59bf0bf2243820087a765935419ecb51
KingKacrae99 <kacrae9@gmail.com>
2025-11-06 23:56:25 +0100

    Initial Commit

--- careercompass/settings.py
+++ careercompass/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY =  config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://careercompass-x8ax.onrender.com']
+ALLOWED_HOSTS = ['careercompass-x8ax.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
