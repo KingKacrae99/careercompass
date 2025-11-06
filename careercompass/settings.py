c777232909f9c35c368b5d77e73af0b8ea5f9a5d
KingKacrae99 <kacrae9@gmail.com>
2025-11-06 23:11:59 +0100

    Initial Commit

--- careercompass/settings.py
+++ careercompass/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gax*55gd-y)=y40ozl&8%tby*u4=q$&hx^-8(s5+_6_i(wi$60'
+SECRET_KEY =  config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG =config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['https://careercompass-x8ax.onrender.com']
 
